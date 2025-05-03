00f2e6caa1286e95232b859455320a7da021d9ea
mourav2222 <mourav2222@gmail.com>
2025-05-03 12:44:45 +0200

    fix the project name in code

--- rococo-e2e/src/test/java/guru/qa/rococo/api/gh/GhApi.java
+++ rococo-e2e/src/test/java/guru/qa/rococo/api/gh/GhApi.java
@@ -9,7 +9,7 @@ import retrofit2.http.Path;
 
 public interface GhApi {
 
-  @GET("repos/dtuchs/heisenbug-2023-autumn/issues/{ISSUE_NUMBER}")
+  @GET("repos/mourav2222/rococo/issues/{ISSUE_NUMBER}")
   @Headers({
       "Accept: application/vnd.github+json",
       "X-GitHub-Api-Version: 2022-11-28"
